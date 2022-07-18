b1ad264bf80645c2b1c981368fc3f938d1a92885
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-07-18 19:13:05 +0100

    update predictive

--- src/Models/initial.jl
+++ src/Models/initial.jl
@@ -63,27 +63,6 @@ function (initialization::PriorInitialization)(_rng::Random.AbstractRNG, kernel,
 end
 
 ############################################################################################
-"""
-$(SIGNATURES)
-Use Prior predictive samples to check model assumptions. Needs dispatch on simulate(rng, model).
-
-# Examples
-```julia
-```
-
-"""
-function predictive(_rng::Random.AbstractRNG, objective::Objective, init::PriorInitialization, iter::Integer)
-    dataᵥ = Vector{typeof(objective.data)}(undef, iter)
-    for idx in Base.OneTo(iter)
-        # Sample from prior for new data points
-        init(_rng, nothing, objective)
-        # Simulate new data
-        dataᵥ[idx] = ModelWrappers.simulate(_rng, objective.model)
-    end
-    return dataᵥ
-end
-
-############################################################################################
 # Export
 export
     AbstractInitialization,
